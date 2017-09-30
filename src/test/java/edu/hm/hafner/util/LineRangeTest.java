c6af509caebf2ddac059d7a648d252ef6d7a4bf0
Ulli Hafner <ullrich.hafner@gmail.com>
2017-09-30 14:05:04 +0200

    Fixed JavaDoc.

--- src/test/java/edu/hm/hafner/util/LineRangeTest.java
+++ src/test/java/edu/hm/hafner/util/LineRangeTest.java
@@ -6,10 +6,23 @@ package edu.hm.hafner.util;
  * @author Ullrich Hafner
  */
 public class LineRangeTest {
+    /**
+     * Creates a new {@link LineRange}. The specified starting and ending lines are included in this range.
+     *
+     * @param from the first line of this range
+     * @param to   the last line of this range
+     * @return the created SUT
+     */
     protected LineRange createLineRange(final int from, final int to) {
         return new LineRange(from, to);
     }
 
+    /**
+     * Creates a new {@link LineRange} containing only one line.
+     *
+     * @param line the single line of this range
+     * @return the created SUT
+     */
     protected LineRange createLineRange(final int line) {
         return new LineRange(line);
     }
