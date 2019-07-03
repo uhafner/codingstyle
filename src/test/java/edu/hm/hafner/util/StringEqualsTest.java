40f91f7e2081cf1e0e38ed72a59c578bc2e8cfb6
Ulli Hafner <ullrich.hafner@gmail.com>
2019-07-03 22:24:58 +0200

    Add some architecture tests.

--- src/test/java/edu/hm/hafner/util/StringEqualsTest.java
+++ src/test/java/edu/hm/hafner/util/StringEqualsTest.java
@@ -1,11 +1,12 @@
 package edu.hm.hafner.util;
 
 /**
- * Example class that shows on how to verify that String instances comply with the contract in {@link Object#equals(Object)}.
+ * Example class that shows on how to verify that String instances comply with the contract in {@link
+ * Object#equals(Object)}.
  *
  * @author Ullrich Hafner
  */
-public class StringEqualsTest extends AbstractEqualsTest {
+class StringEqualsTest extends AbstractEqualsTest {
     @Override
     protected Object createSut() {
         return "Hello World";
