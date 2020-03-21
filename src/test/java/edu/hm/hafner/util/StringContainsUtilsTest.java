c7c00e8b8b4ed276b5b6dd24c5d63ff6ded5b908
unknown <marc.hennig@outlook.com>
2020-03-21 16:09:45 +0100

    Add containsAllIgnoreCase to StringContainsUtils Added static the method containsAllIgnoreCase() to the class StringContainsUtils as well as corresponding tests to verify whether a string contains all strings in a given array.

--- src/test/java/edu/hm/hafner/util/StringContainsUtilsTest.java
+++ src/test/java/edu/hm/hafner/util/StringContainsUtilsTest.java
@@ -9,10 +9,12 @@ import static org.assertj.core.api.Assertions.*;
  * Tests the class {@link StringContainsUtils}.
  *
  * @author Ullrich Hafner
+ * @author Marc Hennig
  */
 class StringContainsUtilsTest {
     @Test
     void shouldHandleNull() {
+        // containsAnyIgnoreCase
         assertThat(containsAnyIgnoreCase("This is a string text.", (String[]) null)).isFalse();
         assertThat(containsAnyIgnoreCase("This is a string text.", (String) null)).isFalse();
         assertThat(containsAnyIgnoreCase("This is a string text.")).isFalse();
@@ -20,15 +22,35 @@ class StringContainsUtilsTest {
         assertThat(containsAnyIgnoreCase(null)).isFalse();
         assertThat(containsAnyIgnoreCase(null, (String) null)).isFalse();
         assertThat(containsAnyIgnoreCase(null, (String[]) null)).isFalse();
+
+        // containsAllIgnoreCase
+        assertThat(containsAllIgnoreCase("This is a string text.", (String[]) null)).isFalse();
+        assertThat(containsAllIgnoreCase("This is a string text.", (String) null)).isFalse();
+        assertThat(containsAllIgnoreCase("This is a string text.")).isFalse();
+
+        assertThat(containsAllIgnoreCase(null)).isFalse();
+        assertThat(containsAllIgnoreCase(null, (String) null)).isFalse();
+        assertThat(containsAllIgnoreCase(null, (String[]) null)).isFalse();
     }
 
     @Test
     void shouldSearchStrings() {
+        // containsAnyIgnoreCase
         assertThat(containsAnyIgnoreCase("This is a string text.", "something")).isFalse();
 
         assertThat(containsAnyIgnoreCase("This is a string text.", "This")).isTrue();
         assertThat(containsAnyIgnoreCase("This is a string text.", "this")).isTrue();
         assertThat(containsAnyIgnoreCase("This is a string text.", "wrong", "is")).isTrue();
         assertThat(containsAnyIgnoreCase("This is a string text.", "wrong", "IS")).isTrue();
+
+        // containsAllIgnoreCase
+        assertThat(containsAllIgnoreCase("This is a string text.", "something")).isFalse();
+
+        assertThat(containsAllIgnoreCase("This is a string text.", "This")).isTrue();
+        assertThat(containsAllIgnoreCase("This is a string text.", "this")).isTrue();
+        assertThat(containsAllIgnoreCase("This is a string text.", "wrong", "is")).isFalse();
+        assertThat(containsAllIgnoreCase("This is a string text.", "wrong", "IS")).isFalse();
+        assertThat(containsAllIgnoreCase("This is a string text.", "is", "text")).isTrue();
+        assertThat(containsAllIgnoreCase("This is a string text.", "is", "TEXT")).isTrue();
     }
 }
