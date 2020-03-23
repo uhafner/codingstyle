893b1ada11dd0cd2c3c6b78397db7fda03f44237
Feyzullah Babur <62477539+FaZehub@users.noreply.github.com>
2020-03-23 19:03:06 +0100

    Ignore Cases
    containsAnyIgnoreCase the add of the true and false cases

--- src/test/java/edu/hm/hafner/util/StringContainsUtilsTest.java
+++ src/test/java/edu/hm/hafner/util/StringContainsUtilsTest.java
@@ -20,12 +20,29 @@ class StringContainsUtilsTest {
         assertThat(containsAnyIgnoreCase(null)).isFalse();
         assertThat(containsAnyIgnoreCase(null, (String) null)).isFalse();
         assertThat(containsAnyIgnoreCase(null, (String[]) null)).isFalse();
+        
+        assertThat(containsAnyIgnoreCase("This is a string text.", (String[]) null)).isFalse();
+        assertThat(containsAnyIgnoreCase("This is a string text.", (String) null)).isFalse();
+        assertThat(containsAnyIgnoreCase("This is a string text.")).isFalse();
+
+        assertThat(containsAnyIgnoreCase(null)).isFalse();
+        assertThat(containsAnyIgnoreCase(null, (String) null)).isFalse();
+        assertThat(containsAnyIgnoreCase(null, (String[]) null)).isFalse();
     }
 
     @Test
     void shouldSearchStrings() {
+        
         assertThat(containsAnyIgnoreCase("This is a string text.", "something")).isFalse();
-
+        
+        assertThat(containsAnyIgnoreCase("This is a string text.", "This")).isTrue();
+        assertThat(containsAnyIgnoreCase("This is a string text.", "wrong", "is")).isTrue();
+        assertThat(containsAnyIgnoreCase("This is a string text.", "wrong", "IS")).isTrue();
+    
+        assertThat(containsAnyIgnoreCase("This is a string text.", "something")).isFalse();
+        assertThat(containsAnyIgnoreCase("This is a string text.", "wrong", "is")).isFalse();
+        assertThat(containsAnyIgnoreCase("This is a string text.", "wrong", "IS")).isFalse();
+        
         assertThat(containsAnyIgnoreCase("This is a string text.", "This")).isTrue();
         assertThat(containsAnyIgnoreCase("This is a string text.", "this")).isTrue();
         assertThat(containsAnyIgnoreCase("This is a string text.", "wrong", "is")).isTrue();
