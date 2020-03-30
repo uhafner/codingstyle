d48ad1d74628dd5548ad9700b857f3c96c8927a4
Philipp Keidler <keidler@hm.edu>
2020-03-30 16:37:56 +0200

    Added function to the tests: "size()" and "isEmpty()"
    Commits before:
    7b47932 Added Imports and a simple test for the constructor
    972bc1e Created the methods for testing.
    f368d68 Created the class SetTest

--- src/test/java/edu/hm/hafner/util/SetTest.java
+++ src/test/java/edu/hm/hafner/util/SetTest.java
@@ -4,7 +4,6 @@ package edu.hm.hafner.util;
 import org.junit.jupiter.api.Test;
 //Use-Case Imports for Unit Test
 import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
 //Import the class who should be tested
 import java.util.Set;
 import java.util.HashSet;
@@ -15,6 +14,9 @@ import java.util.HashSet;
  * @author Philipp Keidler
  */
 public class SetTest {
+    private Set<Integer> testHash;
+    private Set<Integer> extendHash;
+
     @Test
     void constructions() {
         //Test: public HashSet() (standard constructor)
@@ -25,13 +27,65 @@ public class SetTest {
     void iterator() { }
 
     @Test
-    void size() { }
+    void size() {
+        testPrep();
+
+        /* Test: Sizes after declaration. */
+        assertThat(testHash.size()).isEqualTo(0);
+        assertThat(extendHash.size()).isEqualTo(4);
+
+        /* Test: Size after Adding something. */
+        testHash.addAll(extendHash);
+        assertThat(testHash.size()).isEqualTo(4);
+        testHash.add(404);
+        assertThat(testHash.size()).isEqualTo(5);
+        testHash.add(404); //Duplicates are not added.
+        assertThat(testHash.size()).isEqualTo(5);
+
+        /* Test: Size after removing something. */
+        testHash.remove(404);
+        assertThat(testHash.size()).isEqualTo(4);
+        testHash.removeAll(extendHash);
+        assertThat(testHash.size()).isEqualTo(0);
+    }
 
     @Test
-    void isEmpty() { }
+    void isEmpty() {
+        testPrep();
+        /* Test: After creating a fresh Variable */
+        assertThat(testHash.isEmpty()).isTrue();
+
+        /* Test: Adding things (not empty anymore), it has to stay FALSE */
+        testHash.add(511);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.add(2106);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.add(1310);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.add(2401);
+        assertThat(testHash.isEmpty()).isFalse();
+
+        /* Test: Removing thing (until empty), it should getting TRUE after removing the last Object */
+        testHash.remove(511);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.remove(2106);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.remove(1310);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.remove(2401);
+        assertThat(testHash.isEmpty()).isTrue();
+
+        /* Test: Adding and removing more than one Object. Test if isEmpty() acts as expected. */
+        testHash.addAll(extendHash);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.removeAll(extendHash);
+        assertThat(testHash.isEmpty()).isTrue();
+    }
 
     @Test
-    void contains() { }
+    void contains() {
+
+    }
 
     @Test
     void add() { }
@@ -50,4 +104,16 @@ public class SetTest {
 
     @Test
     void readObject() { }
+
+    /**
+     * Sets the test variable to the starting point.
+     */
+    void testPrep() {
+        testHash = new HashSet<>();
+        extendHash = new HashSet<>();
+        extendHash.add(511);
+        extendHash.add(2106);
+        extendHash.add(1310);
+        extendHash.add(2401);
+    }
 }
