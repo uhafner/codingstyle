972bc1e021eb64858a198303514573dd2baa135a
Philipp Keidler <keidler@hm.edu>
2020-03-30 11:17:01 +0200

    Created the methods for testing.
    Commits before:
    f368d68 Created the class SetTest

--- src/test/java/edu/hm/hafner/util/SetTest.java
+++ src/test/java/edu/hm/hafner/util/SetTest.java
@@ -1,10 +1,68 @@
 package edu.hm.hafner.util;
 
+//Standard Imports for Unit Test
+import org.junit.jupiter.api.Test;
+//Use-Case Imports for Unit Test
+//Import the class who should be tested
+
 /**
  * Tests the class HashSet, with usage of SUT Hashsets from Integer.
  *
  * @author Philipp Keidler
  */
 public class SetTest {
+    @Test
+    void constructions() {
 
+    }
+
+    @Test
+    void iterator() {
+
+    }
+
+    @Test
+    void size() {
+
+    }
+
+    @Test
+    void isEmpty() {
+
+    }
+
+    @Test
+    void contains() {
+
+    }
+
+    @Test
+    void add() {
+
+    }
+
+    @Test
+    void remove() {
+
+    }
+
+    @Test
+    void clear() {
+
+    }
+
+    @Test
+    void cloneObject() {
+
+    }
+
+    @Test
+    void writeObject() {
+
+    }
+
+    @Test
+    void readObject() {
+
+    }
 }
