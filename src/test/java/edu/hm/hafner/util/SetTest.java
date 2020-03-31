bea03372ea6901ae7f79e9d3b697367cdcc9ce6e
DON-QS <m.vierheilig0@hm.edu>
2020-03-31 17:00:07 +0200

    ADD new Class SetTest in JUnit

---
+++ src/test/java/edu/hm/hafner/util/SetTest.java
@@ -0,0 +1,17 @@
+package edu.hm.hafner.util;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Iterator;
+
+/**
+ * Test the class {@link java.util.HashSet} for integers.
+ *
+ * @author Markus Vierheilig
+ */
+
+class SetTest {
+
+}
