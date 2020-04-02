1f266951e978f762088a234fd9b3eea67c83c426
Nina-Grabow <grabow@hm.edu>
2020-04-02 09:25:09 +0200

    ADD: SetTest

---
+++ src/test/java/edu/hm/hafner/util/SetTest.java
@@ -0,0 +1,179 @@
+package edu.hm.hafner.util;
+//Standard Imports for Unit Test
+import org.junit.jupiter.api.Test;
+//Imports for Unit Tests
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.Assertions.*;
+//Import the testing classes
+import java.util.Set;
+import java.util.HashSet;
+
+/**
+ * JUnitTests for the class HashSet.
+ *
+ * @author Nina Grabow
+ */
+
+public class SetTest {
+    private Set<Integer> testHash;
+    private Set<Integer> extendHash;
+    final int initialCapacity = 3; // >= 0
+    final float loadFactor = (float) 1.0; // > 0
+
+    @Test
+    void constructions() {
+        /* Test: standard constructor */
+        testHash = new HashSet<Integer>();
+        assertThat(testHash).isNotNull();
+        assertThat(testHash).isEmpty();
+
+        /* Test: HashSet(int initialCapacity) */
+        testHash = new HashSet<>(initialCapacity);
+        assertThat(testHash).isNotNull();
+        assertThat(testHash).isEmpty();
+
+        /* Test: HashSet(int initialCapacity, float loadFactor) */
+        testHash = new HashSet<>(initialCapacity, loadFactor);
+        assertThat(testHash).isNotNull();
+        assertThat(testHash).isEmpty();
+
+        /* Test: HashSet(Collection<? extends E> c) */
+        testInitializing();
+        testHash = new HashSet<>(extendHash);
+        assertThat(testHash).isNotNull();
+        assertThat(testHash).isNotEmpty();
+        testHash.add(3101);
+        assertThat(testHash.size()).isEqualTo(5);
+        assertThat(extendHash.size()).isEqualTo(4); //that shows a deep copy, NOT a pointer copy!
+    }
+
+
+    @Test
+    void iterator() {
+        //Test configuration
+            testHash = new HashSet<>();
+            testHash.add(4);
+            testHash.add(500);
+
+        //Test
+        assertThat(testHash.iterator().hasNext()).isTrue();
+        assertThat(testHash.iterator().next()).isEqualTo(4);
+        testHash.remove(4);
+        assertThat(testHash.iterator().next()).isEqualTo(500);
+        testHash.remove(500);
+        assertThat(testHash.iterator().hasNext()).isFalse();
+        }
+        
+    @Test
+    void size() {
+        testInitializing();
+
+        /* Test: Sizes after declaration. */
+        assertThat(testHash.size()).isEqualTo(0);
+        assertThat(extendHash.size()).isEqualTo(4);
+
+        /* Test: Adding  */
+        testHash.addAll(extendHash);
+        assertThat(testHash.size()).isEqualTo(4);
+        testHash.add(360);
+        assertThat(testHash.size()).isEqualTo(5);
+        testHash.add(360);
+        assertThat(testHash.size()).isEqualTo(5);
+
+        /* Test: Size after removing something. */
+        testHash.remove(360);
+        assertThat(testHash.size()).isEqualTo(4);
+        testHash.removeAll(extendHash);
+        assertThat(testHash.size()).isEqualTo(0);
+    }
+
+    @Test
+    void isEmpty() {
+        testInitializing();
+
+        /* Test: After creating a new variable */
+        assertThat(testHash.isEmpty()).isTrue();
+
+        /* Test: Adding things (not empty anymore), it has to stay FALSE */
+        testHash.add(2503);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.add(1106);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.add(2212);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.add(709);
+        assertThat(testHash.isEmpty()).isFalse();
+
+        /* Test: Removing thing (until empty), it should getting TRUE after removing the last Object */
+        testHash.remove(2503);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.remove(1106);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.remove(2212);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.remove(709);
+        assertThat(testHash.isEmpty()).isTrue();
+
+        /* Test: Adding and removing more than one Object. Test if isEmpty() acts as expected. */
+        testHash.addAll(extendHash);
+        assertThat(testHash.isEmpty()).isFalse();
+        testHash.removeAll(extendHash);
+        assertThat(testHash.isEmpty()).isTrue();
+    }
+
+    @Test
+    void contains() {
+        testInitializing();
+        assertThat(extendHash.contains(1106)).isTrue();
+        assertThat(extendHash.contains(2503)).isTrue();
+        assertThat(extendHash.contains(2212)).isTrue();
+        assertThat(extendHash.contains(709)).isTrue();
+        assertThat(extendHash.contains(4)).isFalse();
+        assertThat(extendHash.contains(0)).isFalse();
+        assertThat(extendHash.contains(1999)).isFalse();
+    }
+
+    @Test
+    void add() {
+    testHash = new HashSet<>();
+    assertThat(testHash).isEmpty();
+    testHash.add(50);
+    assertThat(testHash.hashCode()).isEqualTo(50);
+    testHash.add(443);
+    assertThat(testHash.hashCode()).isEqualTo(493); //50+443
+    }
+
+    @Test
+    void remove() {
+        testInitializing();
+        assertThat(extendHash).isNotNull();
+        assertThat(extendHash.hashCode()).isEqualTo(6530); //All extendedHash integers cumulated
+        extendHash.remove(709);
+        assertThat(extendHash.hashCode()).isEqualTo(5821); //All extendedHash integers cumulated without 709
+        extendHash.remove(1106);
+        extendHash.remove(2503);
+        extendHash.remove(2212);
+        assertThat(extendHash.hashCode()).isEqualTo(0);
+        assertThat(extendHash).isEmpty();
+    }
+
+    @Test
+    void clear() {
+        testInitializing();
+        extendHash.clear();
+        assertThat(extendHash.isEmpty()).isTrue();
+    }
+
+    /**
+     * Test variables initializing
+     */
+    void testInitializing() {
+        testHash = new HashSet<>(); //empty
+        extendHash = new HashSet<>();
+        extendHash.add(1106);
+        extendHash.add(2503);
+        extendHash.add(2212);
+        extendHash.add(709);
+    }
+}
