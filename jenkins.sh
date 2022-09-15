ea077decaff4025af5b4db0f1e0f4e391353a96e
Ulli Hafner <ullrich.hafner@gmail.com>
2022-09-15 14:59:45 +0200

    Bump versions to latest JDK 17 requirements.

--- jenkins.sh
+++ jenkins.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+docker pull jenkins/jenkins:lts-alpine
+
+docker compose build --pull
+
 # Make sure that docker runs with the current UID to avoid permission problems on volume docker/volumes/jenkins-home
 CURRENT_UID="$(id -u):$(id -g)"
 export CURRENT_UID
 echo Running docker compose with user ID $CURRENT_UID
-docker-compose up
+
+docker compose up --always-recreate-deps
