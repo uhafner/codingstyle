bbe54b1f5945aeaa291f1983e94f670f9862c6d1
Ulli Hafner <ullrich.hafner@gmail.com>
2022-09-16 15:18:57 +0200

    Add Java module requirements.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,7 @@
+module codingstyle {
+    requires java.xml;
+    requires org.apache.commons.lang3;
+    requires org.apache.commons.io;
+    requires com.github.spotbugs.annotations;
+    requires com.google.errorprone.annotations;
+}
