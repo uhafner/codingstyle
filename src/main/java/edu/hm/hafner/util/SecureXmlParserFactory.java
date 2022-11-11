5c644251fdc0a7b1130b4cdbbb98a53cbac3557d
Ulli Hafner <ullrich.hafner@gmail.com>
2022-11-11 23:38:10 +0100

    Add new constructors for parsing exception.

--- src/main/java/edu/hm/hafner/util/SecureXmlParserFactory.java
+++ src/main/java/edu/hm/hafner/util/SecureXmlParserFactory.java
@@ -25,6 +25,8 @@ import org.xml.sax.SAXNotRecognizedException;
 import org.xml.sax.SAXNotSupportedException;
 import org.xml.sax.helpers.DefaultHandler;
 
+import com.google.errorprone.annotations.FormatMethod;
+
 import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 
 import static javax.xml.XMLConstants.*;
@@ -343,8 +345,50 @@ public class SecureXmlParserFactory {
          *         the cause (which is saved for later retrieval by the {@link #getCause()} method).
          */
         public ParsingException(final Throwable cause) {
-            super(String.format("%s%n%s%n%s", "Exception occurred during parsing",
-                    ExceptionUtils.getMessage(cause), ExceptionUtils.getStackTrace(cause)), cause);
+            super(createMessage(cause, "Exception occurred during parsing"), cause);
+        }
+
+        /**
+         * Constructs a new {@link ParsingException} with the specified message.
+         *
+         * @param messageFormat
+         *         the message as a format string as described in <a href="../util/Formatter.html#syntax">Format string
+         *         syntax</a>
+         * @param args
+         *         Arguments referenced by the format specifiers in the format string.  If there are more arguments than
+         *         format specifiers, the extra arguments are ignored.  The number of arguments is variable and may be zero.
+         *         The maximum number of arguments is limited by the maximum dimension of a Java array as defined by
+         *         <cite>The Java&trade; Virtual Machine Specification</cite>. The behaviour on a {@code null} argument
+         *         depends on the <a href="../util/Formatter.html#syntax">conversion</a>.
+         */
+        @FormatMethod
+        public ParsingException(final String messageFormat, final Object... args) {
+            super(String.format(messageFormat, args));
+        }
+
+        /**
+         * Constructs a new {@link ParsingException} with the specified cause and message.
+         *
+         * @param cause
+         *         the cause (which is saved for later retrieval by the {@link #getCause()} method).
+         * @param messageFormat
+         *         the message as a format string as described in <a href="../util/Formatter.html#syntax">Format string
+         *         syntax</a>
+         * @param args
+         *         Arguments referenced by the format specifiers in the format string.  If there are more arguments than
+         *         format specifiers, the extra arguments are ignored.  The number of arguments is variable and may be zero.
+         *         The maximum number of arguments is limited by the maximum dimension of a Java array as defined by
+         *         <cite>The Java&trade; Virtual Machine Specification</cite>. The behaviour on a {@code null} argument
+         *         depends on the <a href="../util/Formatter.html#syntax">conversion</a>.
+         */
+        @FormatMethod
+        public ParsingException(final Throwable cause, final String messageFormat, final Object... args) {
+            super(createMessage(cause, String.format(messageFormat, args)), cause);
+        }
+
+        private static String createMessage(final Throwable cause, final String message) {
+            return String.format("%s%n%s%n%s", message,
+                    ExceptionUtils.getMessage(cause), ExceptionUtils.getStackTrace(cause));
         }
     }
 }
