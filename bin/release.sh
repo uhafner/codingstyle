b7cc33b8e0100f0634963ed9dc31950c0e2e7672
Ulli Hafner <ullrich.hafner@gmail.com>
2024-01-23 16:01:04 +0100

    Synchronize line range with analysis-model.

--- bin/release.sh
+++ bin/release.sh
@@ -3,3 +3,4 @@
 git pull
 git push
 mvn -B clean build-helper:parse-version release:prepare release:perform -DdevelopmentVersion=\${parsedVersion.majorVersion}.\${parsedVersion.nextMinorVersion}.0-SNAPSHOT
+mvn -Dproject.version=\${parsedVersion.majorVersion}.\${parsedVersion.minorVersion}.\${parsedVersion.patchVersion} com.github.ferstl:depgraph-maven-plugin:graph scm:add -Dincludes=doc/dependency-graph.puml
--- src/test/java/edu/hm/hafner/util/LineRangeTest.java
+++ src/test/java/edu/hm/hafner/util/LineRangeTest.java
@@ -19,15 +19,31 @@ class LineRangeTest {
     }
 
     @Test
-    void shouldCreateSingleLine() {
-        assertThat(new LineRange(5, 5)).hasStart(5).hasEnd(5).isSingleLine();
-        assertThat(new LineRange(5)).hasStart(5).hasEnd(5).isSingleLine();
+    void shouldFindLinesInsideAndOutsideOfLineRange() {
+        var lineRange = new LineRange(1, 2);
 
-        assertThat(new LineRange(5, 4)).hasStart(4).hasEnd(5).isNotSingleLine();
+        assertThat(lineRange.contains(0)).isFalse();
+        assertThat(lineRange.contains(1)).isTrue();
+        assertThat(lineRange.contains(2)).isTrue();
+        assertThat(lineRange.contains(3)).isFalse();
+        assertThat(lineRange).hasStart(1).hasEnd(2).hasLines(1, 2).hasToString("[1-2]");
+
+        var wrongOrder = new LineRange(2, 1);
+        assertThat(wrongOrder.contains(0)).isFalse();
+        assertThat(wrongOrder.contains(1)).isTrue();
+        assertThat(wrongOrder.contains(2)).isTrue();
+        assertThat(wrongOrder.contains(3)).isFalse();
+        assertThat(wrongOrder).hasStart(1).hasEnd(2).hasLines(1, 2).hasToString("[1-2]");
+
+        var point = new LineRange(2);
+        assertThat(point.contains(1)).isFalse();
+        assertThat(point.contains(2)).isTrue();
+        assertThat(point.contains(3)).isFalse();
+        assertThat(point).hasStart(2).hasEnd(2).hasLines(2).hasToString("[2-2]");
     }
 
     @Test
-    void shouldObeyEqualsContract() {
-        EqualsVerifier.simple().forClass(LineRange.class).verify();
+    void shouldAdhereToEquals() {
+        EqualsVerifier.forClass(LineRange.class).verify();
     }
 }
