700343fb6dccc4ec875aaf28abe0a19cd93e4c81
Ulli Hafner <ullrich.hafner@gmail.com>
2024-07-31 15:26:04 +0200

    Fix docker image permissions for .m2 folder.

--- bin/jenkins.sh
+++ bin/jenkins.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+# Make sure that docker runs with the current UID to avoid permission problems on volume docker/volumes/jenkins-home
+CURRENT_UID="$(id -u)"
+export CURRENT_UID
+
+CURRENT_GID="$(id -g)"
+export CURRENT_GID
+
+CURRENT_USER="$(id -u):$(id -g)"
+export CURRENT_USER
+
+echo Running docker compose with user ID $CURRENT_USER
 
 docker pull jenkins/jenkins:lts-jdk21
-
 docker compose build --pull
-
-# Make sure that docker runs with the current UID to avoid permission problems on volume docker/volumes/jenkins-home
-CURRENT_UID="$(id -u):$(id -g)"
-export CURRENT_UID
-echo Running docker compose with user ID $CURRENT_UID
-
 docker compose up --always-recreate-deps
